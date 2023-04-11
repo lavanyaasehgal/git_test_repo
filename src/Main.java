6a59c484ec1aafdbd8c489bfb8da85ce5d0f0274
Lavanya Sehgal <lavanyaasehgal@gmail.com>
2023-04-11 10:20:18 +0530

    trial

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         MallardDuck mallardDuck= new MallardDuck();
+        System.out.println("\nMallard Duck\n");
         mallardDuck.display();
         mallardDuck.performFly();
         mallardDuck.performQuack();
         mallardDuck.performSwim();
 
         RubberDuck rubberDuck= new RubberDuck();
+        System.out.println("\nRubber Duck\n");
         rubberDuck.display();
         rubberDuck.performFly();
         rubberDuck.performQuack();
         rubberDuck.performSwim();
 
         DecoyDuck decoyDuck= new DecoyDuck();
+        System.out.println("\nDecoy Duck\n");
         decoyDuck.display();
         decoyDuck.performFly();
         decoyDuck.performQuack();
